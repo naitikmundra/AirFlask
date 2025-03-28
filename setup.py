9580394cc2f57357d15640082ce2f3bafabce7d9
naitikmundra <naitikmundra18@gmail.com>
2025-03-28 10:13:56 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="airflask",  
-    version="0.0.1",
+    version="0.1.0",
     author="Naitik Mundra",
     author_email="naitikmundra18@gmail.com",
+    license="MIT",
     description="Simplest way to host your flask web app in production!",
     url="https://github.com/naitikmundra/AirFlask",
     packages=find_packages(), 
